9e2315ada60f1cfd35f6ea8951e46ab1a82db3cc
ffenland <ffenland@gmail.com>
2021-12-27 16:30:20 +0900

    3rd

--- services/boripharms.js
+++ services/boripharms.js
@@ -43,7 +43,6 @@ export async function boirBag() {
       Accept:
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       Cookie:
-        "boripharma=align=phy&memCd=16&venNm=%EB%B3%B4%EB%9E%8C%EC%95%BD%EA%B5%AD%28%EC%A3%BC%EB%AC%B8%EC%A7%84%29&venTel=033%2D662%2D8242&LoginNm=%EA%B9%80%EB%AF%BC%EC%A7%80&LoginVenNm=%EB%B3%B4%EB%9E%8C%EC%95%BD%EA%B5%AD%28%EC%A3%BC%EB%AC%B8%EC%A7%84%29&salesNm=%EB%B0%B1%EC%8A%B9%ED%98%84&memNm=%EA%B9%80%EB%AF%BC%EC%A7%80&salesPhone=&venCd=50169&memId=pholy9on&memKind=U&stockCd=&listCnt=10&venGroup=9999",
     },
   };
 
