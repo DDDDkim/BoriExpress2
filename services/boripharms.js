9dbfdfb28a9c0cb43c3c6c8497aca942d6aa76eb
ffenland <ffenland@gmail.com>
2021-12-27 16:29:40 +0900

    2nd

--- services/boripharms.js
+++ services/boripharms.js
@@ -29,3 +29,29 @@ export async function boriLogin(id, pw) {
 
   return cookie;
 }
+
+export async function boirBag() {
+  var axios = require("axios");
+
+  var config = {
+    method: "get",
+    url: "http://boripharma.com/Service/Order/Bag.asp?currVenCd=50169&currMkind=",
+    headers: {
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      Cookie:
+        "boripharma=align=phy&memCd=16&venNm=%EB%B3%B4%EB%9E%8C%EC%95%BD%EA%B5%AD%28%EC%A3%BC%EB%AC%B8%EC%A7%84%29&venTel=033%2D662%2D8242&LoginNm=%EA%B9%80%EB%AF%BC%EC%A7%80&LoginVenNm=%EB%B3%B4%EB%9E%8C%EC%95%BD%EA%B5%AD%28%EC%A3%BC%EB%AC%B8%EC%A7%84%29&salesNm=%EB%B0%B1%EC%8A%B9%ED%98%84&memNm=%EA%B9%80%EB%AF%BC%EC%A7%80&salesPhone=&venCd=50169&memId=pholy9on&memKind=U&stockCd=&listCnt=10&venGroup=9999",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
